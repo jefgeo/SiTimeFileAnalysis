371938c580f1f2f0a79eee802224b24d4c63f287
Jeff George <jefgeo@users.noreply.github.com>
2022-05-26 18:05:05 -0400

    Misc fixes and enhancements Updated some files to function properly (removed rows with headers, etc). Updated stats to include int and datetime Added Inferred_type column to summary

--- SiTime.py
+++ SiTime.py
@@ -1,83 +1,73 @@
 import pandas as pd
 import os
-
-def report():
-    print("Summary of {}".format(filename))
-    print("Potential Keys:  {}".format(', '.join([str(elem) for elem in possible_keys])))
-    print("Top 5 Values by Column")
-    for k, v in top_freq.items():
-        if k in possible_keys:
-            k += '    *****Possible Key'
-        print("\nColumn:  {}".format(k))
-        for k2, v2, in v.items():
-            print('{0:<50}{1:<10}'.format(k2, v2))
-
-
-id_list = ["id", "key", "pk", "fk"]
-not_id = ["middle"]
+from openpyxl import load_workbook
 
 writer = pd.ExcelWriter("summary.xlsx")
 
 directory = './Data/'
 
-summary_columns = ['File_Name', 'Column_Name',
+summary_columns = ['File_Name', 'Column_Name', 'Inferred_Type',
                    'Populated', 'Uniuqe_Values', 'PctUnique',
                    'Nulls', 'PctMissing',
                    'Top_Values', 'mean', 'median', 'max', 'min']
 
 for file_name in os.listdir(directory):
 
-    if file_name.endswith(".csv") or file_name.endswith(".xlsx"):
+    if file_name.lower().endswith(".csv") or file_name.lower().endswith(".xlsx"):
         print(file_name)
-        possible_keys = []
-        if file_name.endswith(".csv"):
-            df = pd.read_csv('{}{}'.format(directory, file_name), low_memory=False, quotechar='"')
-        elif file_name.endswith(".xls") or file_name.endswith(".xlsx"):
-            df = pd.read_excel('{}{}'.format(directory, file_name))
+
+        if file_name.lower().endswith(".csv"):
+            sheet_list = ['.csv']
+        elif file_name.lower().endswith(".xls") or file_name.lower().endswith(".xlsx"):
+            xls = load_workbook('{}{}'.format(directory, file_name), read_only=True, keep_links=False)
+            sheet_list = xls.sheetnames
 
         summary = []
 
-        top_freq = {}
+        for sheet in sheet_list:
+            print(sheet)
+            if file_name.lower().endswith(".csv"):
+                df = pd.read_csv('{}{}'.format(directory, file_name), low_memory=False, quotechar='"')
+            else:
+                df = pd.read_excel('{}{}'.format(directory, file_name), sheet_name=sheet)
 
-        for col in df:
-            col_name = df[col].name
-            col_name_lower = col_name.lower()
-            col_counts = df[col].value_counts().to_dict()
+            top_freq = {}
 
-            stats = {}
-            if df[col].dtypes == "float64":
-                stats["mean"] = (df[col].mean())
-                stats["min"] = (df[col].min())
-                stats["max"] = (df[col].max())
-                stats["median"] = (df[col].median())
-            #                print(stats)
-            #            else:
-            #                col_counts_nocase = df[col].str.lower().value_counts().to_dict()
-            #                unique_values_nocase = len(col_counts_nocase)
+            for col in df:
+                col_name = df[col].name
+                col_name_lower = col_name.lower()
+                col_counts = df[col].value_counts().to_dict()
 
-            unique_values = len(col_counts)
+                stats = {}
+                if df[col].dtypes in ["float64", "int64", "datetime64[ns]"]:
+                    stats["mean"] = (df[col].mean())
+                    stats["min"] = (df[col].min())
+                    stats["max"] = (df[col].max())
+                    stats["median"] = (df[col].median())
 
-            missing = df[col].isnull()
-            missing_count = 0
-            for v in missing:
-                if v:
-                    missing_count += 1
+                unique_values = len(col_counts)
 
-            {k: v for k, v in sorted(col_counts.items(), key=lambda item: item[1])}
-            top_values = list(col_counts)[:unique_values if unique_values < 10 else 5]
+                missing = df[col].isnull()
+                missing_count = 0
+                for v in missing:
+                    if v:
+                        missing_count += 1
 
-            entry = [file_name, col_name_lower, len(df) - missing_count,
-                     unique_values, unique_values / len(df),
-                     missing_count, missing_count / len(df),
-                     top_values,
-                     (stats["mean"] if "mean" in stats else 'na'),
-                    (stats["median"] if "median" in stats else 'na'),
-                     (stats["min"] if "min" in stats else 'na'),
-                     (stats["max"] if "max" in stats else 'na')]
-            summary.append(entry)
+                {k: v for k, v in sorted(col_counts.items(), key=lambda item: item[1])}
+                top_values = list(col_counts)[:unique_values if unique_values < 10 else 5]
 
-        # print(summary)
+                entry = [file_name+' | '+sheet if sheet != '.csv' else file_name,
+                        col_name_lower, df[col].dtypes,
+                        len(df) - missing_count,
+                        unique_values, unique_values / len(df),
+                        missing_count, missing_count / len(df),
+                        top_values,
+                        (stats["mean"] if "mean" in stats else ''),
+                        (stats["median"] if "median" in stats else ''),
+                        (stats["min"] if "min" in stats else ''),
+                        (stats["max"] if "max" in stats else '')]
+                summary.append(entry)
+
         summary_df = pd.DataFrame(summary, columns=summary_columns)
-        # print(summary_df)
         summary_df.to_excel(writer, header=True, sheet_name=file_name[0:30], index=False)
 writer.close()
