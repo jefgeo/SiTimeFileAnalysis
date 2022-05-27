e8f8d35d13d05eb6b052ef978c2a8bfc60d4dace
Jeff George <jefgeo@users.noreply.github.com>
2022-05-27 15:21:03 -0400

    Excel number formatting.

--- SiTime.py
+++ SiTime.py
@@ -2,6 +2,9 @@ import pandas as pd
 import os
 from openpyxl import load_workbook
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 100)
+
 writer = pd.ExcelWriter("summary.xlsx")
 
 directory = './Data/'
@@ -9,27 +12,23 @@ directory = './Data/'
 summary_columns = ['File_Name', 'Column_Name', 'Inferred_Type',
                    'Populated', 'Unique_Values', 'PctUnique',
                    'Nulls', 'PctMissing',
-                   'Top_Values', 'mean', 'median', 'max', 'min']
-
+                   'Top_Values', 'mean', 'median', 'min', 'max']
 file_count = 0
 sheet_count = 0
 for file_name in os.listdir(directory):
+
+    summary = []
 
     if file_name.lower().endswith(".csv") \
             or file_name.lower().endswith(".xlsx") \
             or file_name.lower().endswith(".xls"):
         print(file_name)
         file_count += 1
-        if file_name.lower().endswith(".csv"):
-            sheet_list = ['.csv']
-        elif file_name.lower().endswith(".xlsx"):
+        if file_name.lower().endswith(".xlsx"):
             xls = load_workbook('{}{}'.format(directory, file_name), read_only=True, keep_links=False)
             sheet_list = xls.sheetnames
-        elif file_name.lower().endswith(".xls"):
-            sheet_list = ['.xls']
-
-
-        summary = []
+        else:
+            sheet_list = ['__Single_Sheet__']
 
         for sheet in sheet_list:
             sheet_count += 1
@@ -66,19 +65,35 @@ for file_name in os.listdir(directory):
                 {k: v for k, v in sorted(col_counts.items(), key=lambda item: item[1])}
                 top_values = list(col_counts)[:unique_values if unique_values < 10 else 5]
 
-                entry = [file_name+' | '+sheet if sheet != '.csv' else file_name,
-                        col_name_lower, df[col].dtypes,
-                        len(df) - missing_count,
-                        unique_values, unique_values / len(df) if len(df) > 0 else 0,
-                        missing_count, missing_count / len(df) if len(df) > 0 else 0,
-                        top_values,
-                        (stats["mean"] if "mean" in stats else ''),
-                        (stats["median"] if "median" in stats else ''),
-                        (stats["min"] if "min" in stats else ''),
-                        (stats["max"] if "max" in stats else '')]
+                entry = [file_name + ' | ' + sheet if sheet != '__Single_Sheet__' else file_name,
+                         col_name_lower, df[col].dtypes,
+                         len(df) - missing_count,
+                         unique_values, unique_values / len(df) if len(df) > 0 else 0,
+                         missing_count, missing_count / len(df) if len(df) > 0 else 0,
+                         top_values,
+                         (stats["mean"] if "mean" in stats else ''),
+                         (stats["median"] if "median" in stats else ''),
+                         (stats["min"] if "min" in stats else ''),
+                         (stats["max"] if "max" in stats else '')]
                 summary.append(entry)
 
         summary_df = pd.DataFrame(summary, columns=summary_columns)
         summary_df.to_excel(writer, header=True, sheet_name=file_name[0:30], index=False)
+
+        percentage_format = writer.book.add_format({'num_format': '0%'})
+        int_format = writer.book.add_format({'num_format': '#,##0'})
+        float_format = writer.book.add_format({'num_format': '#,##0.00'})
+
+        for column in summary_df:
+            column_width = max(summary_df[column].astype(str).map(len).max(), len(column))
+            col_idx = summary_df.columns.get_loc(column)
+            writer.sheets[file_name[0:30]].set_column(col_idx, col_idx, column_width)
+            if "pct" in column.lower():
+                writer.sheets[file_name[0:30]].set_column(col_idx, col_idx, column_width, percentage_format)
+            elif any(x in column.lower() for x in ['mean', 'median', 'max', 'min']):
+                writer.sheets[file_name[0:30]].set_column(col_idx, col_idx, column_width + 3, float_format)
+            elif summary_df[column].dtypes == "int64":
+                writer.sheets[file_name[0:30]].set_column(col_idx, col_idx, column_width, int_format)
+
 print('Processed {} Files with a total of {} Sheets'.format(file_count, sheet_count))
 writer.close()
